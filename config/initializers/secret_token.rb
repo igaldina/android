5162626c13bd5860dad4166dfbe5e44f7f28263c
Galdina Irina <galdinairina@gmail.com>
2014-01-16 11:55:10 +0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AndroidApp::Application.config.secret_key_base = '6cc67c675ea53561f18ddc01212e3f6acb1e9b0e59268537d070549045c8fc7a5917757da05c869ef2d0de74b7b8618ecdddfdac66c5ee42b825ef026bc58167'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AndroidApp::Application.config.session_store :cookie_store, key: '_android_app_session'
